8b9a380b8059802b98531ca876f3cf6cf144e871
Gabesz Rózsa <gabesz.rozsa@gmail.com>
2018-12-12 20:39:46 +0100

    add UserRepository

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,21 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
 import User from '../models/User';
 import { ICurrentUser, IUser, IRegisterUser, ILoginUser } from 'entities';
+import { UserRepository } from '../repositories';
 
 export default class UserController {
   public logOut(req: Request, res: Response) {
-    req.body.user.removeToken(req.body.token)
-    .then(() => {
-      res.status(200).send();
-    }, () => {
-      res.status(400).send();
-    });
+    UserRepository.logOut(req.body.user, req.body.token)
+      .then(() => res.status(200).send())
+      .catch((error) => res.status(400).send(error))
   }
 
   public getCurrent(req: Request, res: Response) {
-    const { _id, email, displayName, token } = req.body.user;
-    const currentUser: ICurrentUser = { _id, email, displayName, token };
-    res.send(currentUser);
+    UserRepository.getCurrent(req.body.user)
+      .then((currentUser) => res.send(currentUser))
+      .catch((error) => res.status(400).send(error))
   }
 
   public addNewUser(req: Request, res: Response) {
@@ -25,28 +23,16 @@ export default class UserController {
       displayName: req.body.displayName
     };
 
-    const newUser = new User(registerUser);
-
-    newUser.save()
-      .then(() => newUser.generateAuthToken())
-      .then(token => {
-        const { _id, displayName, email } = newUser;
-        const user: ICurrentUser = { _id, displayName, email, token };
-
-        res.header('x-auth', token).send(user);
-      })
+    UserRepository.addNewUser(registerUser)
+      .then(({ token, user }) => res.header('x-auth', token).send(user))
       .catch(error => res.status(400).send(error));
   }
 
   public getAllUsers(req: Request, res: Response) {
-    User.find({}, (err, result) => {
-      if(err){
-          res.send(err);
-      }
-
-      const users = result.map(u => (<IUser>{ _id: u._id, displayName: u.displayName, email: u.email }));
-      res.json(users);
-    });
+    
+    UserRepository.getAllUsers()
+      .then((users) => res.json(users))
+      .catch(error => res.status(400).send(error));
   }
 
   public loginWithUser(req: Request, res: Response) {
@@ -55,26 +41,15 @@ export default class UserController {
       password: req.body.password
     };
 
-    User.findByCredentials(loginUser)
-      .then(user => user.generateAuthToken()
-        .then(token => {
-          const { _id, displayName, email } = user;
-          const currentUser: ICurrentUser = { _id, displayName, email, token };
-          res.header('x-auth', token).send(currentUser)
-        })
-      )
-      .catch(e => res.status(400).send());
+    UserRepository.loginWithUser(loginUser)
+      .then(({ token, currentUser }) => res.header('x-auth', token).send(currentUser))
+      .catch(error => res.status(400).send(error));
   }
 
   public authenticate(req: Request, res: Response, next: NextFunction) {
     const token = req.header('x-auth');
-    User.findByToken(token)
+    UserRepository.authenticate(token)
       .then(user => {
-        if (!user) {
-          console.log('[ERROR] UserController -> authenticate: no user with token', token);
-          return Promise.reject();
-        }
-
         req.body.user = user;
         req.body.token = token;
         next();
--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -1,3 +1,115 @@
+import User from '../models/User';
+import { ICurrentUser, IUser, ILoginUser } from '../entities';
+
 export default class UserRepository {
+
+  public static logOut(user, token: string) {
+    return new Promise((resolve, reject) => {
+      user.removeToken(token)
+      .then(() => resolve())
+      .catch(error => {
+        console.log('[ERROR] - UserRepository :: logOut');
+        console.log('user | ', user);
+        console.log('token | ', token);
+        console.log(error);
+        reject(error);
+      })
+    });
+  }
+
+  public static getCurrent(user) {
+    return new Promise((resolve, reject) => {
+      try {
+        const { _id, email, displayName, token } = user;
+        const currentUser: ICurrentUser = { _id, email, displayName, token };
   
+        resolve(currentUser);
+      } catch (error) {
+          console.log('[ERROR] - UserRepository :: getCurrent');
+          console.log('user | ', user);
+          console.log(error);
+          reject(error);
+      }
+    });
+  }
+
+  public static addNewUser(user) {
+    return new Promise((resolve, reject) => {
+      const newUser = new User(user);
+  
+      newUser.save()
+        .then(() => newUser.generateAuthToken())
+        .then(token => {
+          const { _id, displayName, email } = newUser;
+          const user: ICurrentUser = { _id, displayName, email, token };
+          resolve({ token, user })
+        })
+        .catch(error => {
+          console.log('[ERROR] - UserRepository :: addNewUser');
+          console.log('user | ', user);
+          console.log(error);
+          reject(error);
+        });
+    });
+  }
+
+  public static getAllUsers() {
+    return new Promise((resolve, reject) => {
+      User
+        .find({})
+        .exec()
+        .then((result) => {
+          const users: IUser[] = result
+            .map(u => <IUser>{ _id: u._id, displayName: u.displayName, email: u.email });
+            
+          resolve(users);
+        })
+        .catch(error => {
+          console.log('[ERROR] - UserRepository :: getAllUsers');
+          console.log(error);
+          reject(error)
+        });
+    });
+  }
+
+  public static loginWithUser(loginUser: ILoginUser) {
+    return new Promise((resolve, reject) => {
+
+      User.findByCredentials(loginUser)
+      .then(user => user.generateAuthToken()
+        .then(token => {
+          const { _id, displayName, email } = user;
+          const currentUser: ICurrentUser = { _id, displayName, email, token };
+          resolve({ token, currentUser });
+        })
+      )
+      .catch(error => {
+        console.log('[ERROR] - UserRepository :: loginWithUser');
+        console.log('loginUser | ', loginUser);
+        console.log(error);
+        reject(error)
+      });
+
+    });
+  }
+
+  public static authenticate(token: string) {
+    return new Promise((resolve, reject) => {
+      User.findByToken(token)
+        .then(user => {
+          if (!user) {
+            console.log('[ERROR] UserRepository :: authenticate: no user with token', token);
+            reject();
+          }
+
+          resolve(user);
+        })
+        .catch(error => {
+          console.log('[ERROR] - UserRepository :: authenticate');
+          console.log('token | ', token);
+          console.log(error);
+          reject(error)
+        });
+    });
+  }
 }
