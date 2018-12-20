434656e4a5d4f6566f86a024b916a9adda3da564
Gabor Rozsa <gabor.rozsa@blackbelt.hu>
2018-12-20 14:31:10 +0100

    remove dist from server

--- dist/config/dev.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MONGO_URL = 'mongodb://localhost/Astro';
-exports.JWT_SECRET = 'secret';
-//# sourceMappingURL=dev.js.map
--- dist/config/test.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MONGO_URL = 'mongodb://localhost/AlacrityTest';
-exports.JWT_SECRET = 'secret';
-//# sourceMappingURL=test.js.map
